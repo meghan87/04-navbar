4ec5326059fa9c53ad6facd374878db427affa1e
meghan87 <115980448+meghan87@users.noreply.github.com>
2022-11-17 11:26:41 +0530

    final commit

--- setup/app.js
+++ setup/app.js
@@ -4,3 +4,11 @@
 // remove - remove class
 // toggle - toggles class
 
+
+const navBtn = document.querySelector(".nav-toggle");
+
+const links = document.querySelector(".links");
+
+navBtn.addEventListener("click",function(){
+    links.classList.toggle("show-links")
+})
